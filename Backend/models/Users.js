79ec460efb8924760566f72ec4969a673322b40f
Agastya Hospitals <dm.agastyahospitals@gmail.com>
2025-11-22 18:41:53 +0530

    Update Users.js

--- Backend/models/Users.js
+++ Backend/models/Users.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
   countryCode: { type: String, required: true }, 
   mobile: { type: String, required: true },
   isActive: { type: Boolean, required: true },
-  roleID: { type: Number, default: 3 },                // default roleID
+  roleID: { type: Number, default: 4 },                // default roleID
   modules: { type: [Number], default: [5, 14] },
   whatsAppNumber: { type: String }
 }, {
