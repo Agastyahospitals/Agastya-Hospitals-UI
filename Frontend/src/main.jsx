87199f1b7a11e05b33bd5dcddf6ec029f69b77ad
Pavan <yengalwarpavan@gmail.com>
2025-12-20 22:50:57 +0530

    Replaced browserrouter to hashrouter for frontend app

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
@@ -9,10 +9,10 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
