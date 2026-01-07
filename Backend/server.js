b3433f7e4fe15318158c8fe56635471b384ea5a8
Pavan <yengalwarpavan@gmail.com>
2026-01-07 08:59:11 +0530

    increasing number of requests to avoid 429 too many requests

--- Backend/server.js
+++ Backend/server.js
@@ -42,7 +42,7 @@ app.use(bodyParser.json());
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100,
+  max: 1000,
   message: 'Too many requests from this IP, please try again later.'
 });
 app.use(limiter);
